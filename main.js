536cfad16a1f015f0962d8127e7a187bb082b152
LeoTovell <51256511+LeoTovell@users.noreply.github.com>
2023-01-22 13:43:29 +0000

    Updates to parsing

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 let draggableDiv = document.createElement("div");
 let title = document.createElement("h1")
 let description = document.createElement("p");
+let preSkinName = document.createElement("p");
 let skinName = document.createElement("p");
 let hint = document.createElement("p");
 let disclaimer = document.createElement("p");
 hint.innerHTML = "<i>Result is automatically copied to clipboard<i>"
-skinName.id = "skin_name"
-skinName.style.backgroundColor = "red"
 description.innerHTML = "<i>This lil script was made by FarmerJoe</i>"
 title.innerHTML = "<strong>CS SKIN HACK!!</strong>"
 disclaimer.innerHTML = "<i>This may not be accurate</i>"
 
-draggableDiv.append(title, description, document.createElement("hr"), skinName, document.createElement("hr"), hint, disclaimer)
+draggableDiv.append(title, description, document.createElement("hr"), preSkinName, skinName, document.createElement("hr"), hint, disclaimer)
 draggableDiv.id = "draggableDiv"
 draggableDiv.style.display = "block";
 
@@ -28,6 +27,11 @@ draggableDiv.style.textAlign = "center"
 draggableDiv.style.border = "4px solid green"
 draggableDiv.style.fontFamily = "Arial, Helvetica, sans-serif"
 draggableDiv.style.cursor = "move"
+
+preSkinName.style.backgroundColor = "red"
+preSkinName.style.padding = "5px"
+skinName.style.backgroundColor = "green"
+skinName.style.padding = "5px"
 
 dragElement(draggableDiv);
 
@@ -84,7 +88,7 @@ function toggleDraggableDiv(){
 
 let clientX, clientY, imageURL, returnedresponse, skin;
 
-let weapons = ["factory new", "factorynew", "factory-new", "minimal-wear", "minimal wear", "min-wear", "min wear", "minimalwear", "field-tested", "field tested", "fieldtested", "well-worn", "well worn", "wellworn", "battle-scarred", "battle scarred", "battlescarred", "cs go", "cs-go", "csgo", "usp-s", "usp s", "usps", "glock-18", "glock 18", "glock18", "dual berettas", "dual-berettas", "p250", "tec-9", "tec9", "tec 9", "cz75-auto", "cz75 auto", "five-seven", "five seven", "desert eagle", "r8 revolver", "mac-10", "mac 10", "mp9", "mp-9", "mp7", "mp-7", "mp5-sd", "mp5 sd", "mp5sd", "ump-45", "ump45", "p90", "p-90", "pp-bizon", "ppbizon", "pp bizon", "nova", "xm1014", "sawed-off", "sawed off", "mag-7", "mag 7", "mag7", "m249", "negev", "galil-ar", "galil ar", "galilar", "famas", "ak-47", "ak47", "ak 47", "m4a4", "m4a1-s", "m4a1", "m4a1 s", "ssg-08", "ssg08", "ssg 08", "scout", "sg-553", "sg 553", "sg553", "aug", "awp", "g3sg1", "scar-20", "scar20", "scar 20", "skeleton knife", "skeleton", "bayonet knife", "bayonet", "bowie knife", "bowie", "butterfly knife", "butterfly", "knife", "classic knife", "falchion knife", "falchion", "gut knife", "hunstmas knife", "karambit", "navaja", "m9 bayonet", "nomad knife", "paracord", "shadow daggers", "stiletto", "survival", "talon", "ursus", "glock"]
+let weapons = ["ump", "stattrak", "™", "skin", "factory new", "factorynew", "factory-new", "minimal-wear", "minimal wear", "min-wear", "min wear", "minimalwear", "field-tested", "field tested", "fieldtested", "well-worn", "well worn", "wellworn", "battle-scarred", "battle scarred", "battlescarred", "cs go", "cs-go", "csgo", "usp-s", "usp s", "usps", "glock-18", "glock 18", "glock18", "dual berettas", "dual-berettas", "p250", "tec-9", "tec9", "tec 9", "cz75-auto", "cz75 auto", "five-seven", "five seven", "desert eagle", "r8 revolver", "mac-10", "mac 10", "mp9", "mp-9", "mp7", "mp-7", "mp5-sd", "mp5 sd", "mp5sd", "ump-45", "ump45", "p90", "p-90", "pp-bizon", "ppbizon", "pp bizon", "nova", "xm1014", "sawed-off", "sawed off", "mag-7", "mag 7", "mag7", "m249", "negev", "galil-ar", "galil ar", "galilar", "famas", "ak-47", "ak47", "ak 47", "m4a4", "m4a1-s", "m4a1", "m4a1 s", "ssg-08", "ssg08", "ssg 08", "scout", "sg-553", "sg 553", "sg553", "aug", "awp", "g3sg1", "scar-20", "scar20", "scar 20", "skeleton knife", "skeleton", "bayonet knife", "bayonet", "bowie knife", "bowie", "butterfly knife", "butterfly", "knife", "classic knife", "falchion knife", "falchion", "gut knife", "hunstmas knife", "karambit", "navaja", "m9 bayonet", "nomad knife", "paracord", "shadow daggers", "stiletto", "survival", "talon", "ursus", "glock"]
 // ToDO add knives
 
 document.addEventListener("mousemove", (e) => {
@@ -92,7 +96,10 @@ document.addEventListener("mousemove", (e) => {
     clientY = e.clientY;
 })
 
+let running = false;
+
 document.body.addEventListener("keydown", (e) => {
+    if(!running){
     let key = e.key;
     if(key = "Delete"){
         elements = document.elementsFromPoint(clientX, clientY);
@@ -101,14 +108,17 @@ document.body.addEventListener("keydown", (e) => {
         while (i < len){
             el = elements[i];
             if(el.tagName == "IMG"){
+                running = true
                 console.log("CSSKIN HACk ------- Querying!")
                 skinName.innerHTML = ""
+                preSkinName.innerHTML = "";
                 imageURL = el.src;
                 sendData(imageURL)
             }
             i++;
         }
     }
+}
 })
 
 function sendData(url){
@@ -132,13 +142,14 @@ function sendData(url){
         returnedresponse = jsonResponse
 
         // Parse and remove wepaon names:
+        preSkinName.innerHTML = `Raw: ${returnedresponse}`
         weapons.forEach(weapon => {
             returnedresponse = returnedresponse.replace(weapon, "")
         });
-        console.log(returnedresponse)
         navigator.clipboard.writeText(returnedresponse)
-        skinName.innerHTML = returnedresponse;
+        skinName.innerHTML = `Final: ${returnedresponse}`;
         console.log(" -____--- PLEASE PASTE INTO ENTRY BOX --___-----")
+        running = false;
     }
     ).catch((err) => console.error(err));
 }
