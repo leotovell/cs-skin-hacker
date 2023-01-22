48cf3a519ba42f374d36a3724acd8dc0607e7565
LeoTovell <51256511+LeoTovell@users.noreply.github.com>
2023-01-22 19:34:45 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 let draggableDiv = document.createElement("div");
 let title = document.createElement("h1")
 let description = document.createElement("p");
-let preSkinName = document.createElement("p");
+// let preSkinName = document.createElement("p");
 let skinName = document.createElement("p");
 let hint = document.createElement("p");
-let disclaimer = document.createElement("p");
+// let disclaimer = document.createElement("p");
 hint.innerHTML = "<i>Result is automatically copied to clipboard<i>"
 description.innerHTML = "<i>This lil script was made by FarmerJoe</i>"
 title.innerHTML = "<strong>CS SKIN HACK!!</strong>"
-disclaimer.innerHTML = "<i>This may not be accurate</i>"
+// disclaimer.innerHTML = "<i>This may not be accurate</i>"
 
-draggableDiv.append(title, description, document.createElement("hr"), preSkinName, skinName, document.createElement("hr"), hint, disclaimer)
+draggableDiv.append(title, description, document.createElement("hr"), skinName, document.createElement("hr"), hint)
 draggableDiv.id = "draggableDiv"
 draggableDiv.style.display = "block";
 
@@ -28,8 +28,8 @@ draggableDiv.style.border = "4px solid green"
 draggableDiv.style.fontFamily = "Arial, Helvetica, sans-serif"
 draggableDiv.style.cursor = "move"
 
-preSkinName.style.backgroundColor = "red"
-preSkinName.style.padding = "5px"
+// preSkinName.style.backgroundColor = "red"
+// preSkinName.style.padding = "5px"
 skinName.style.backgroundColor = "green"
 skinName.style.padding = "5px"
 
@@ -88,7 +88,7 @@ function toggleDraggableDiv(){
 
 let clientX, clientY, imageURL, returnedresponse, skin;
 
-let weapons = ["ump", "stattrak", "™", "skins", "skin", "factory new", "factorynew", "factory-new", "minimal-wear", "minimal wear", "min-wear", "min wear", "minimalwear", "field-tested", "field tested", "fieldtested", "well-worn", "well worn", "wellworn", "battle-scarred", "battle scarred", "battlescarred", "cs go", "cs-go", "csgo", "usp-s", "usp s", "usps", "glock-18", "glock 18", "glock18", "dual berettas", "dual-berettas", "p250", "tec-9", "tec9", "tec 9", "cz75-auto", "cz75 auto", "five-seven", "five seven", "desert eagle", "r8 revolver", "revolver", "mac-10", "mac 10", "mp9", "mp-9", "mp7", "mp-7", "mp5-sd", "mp5 sd", "mp5sd", "ump-45", "ump45", "p90", "p-90", "pp-bizon", "ppbizon", "pp bizon", "nova", "xm1014", "sawed-off", "sawed off", "mag-7", "mag 7", "mag7", "m249", "negev", "galil-ar", "galil ar", "galilar", "famas", "ak-47", "ak47", "ak 47", "m4a4", "m4a1-s", "m4a1", "m4a1 s", "ssg-08", "ssg08", "ssg 08", "scout", "sg-553", "sg 553", "sg553", "aug", "awp", "g3sg1", "scar-20", "scar20", "scar 20", "skeleton knife", "skeleton", "bayonet knife", "bayonet", "bowie knife", "bowie", "butterfly knife", "butterfly", "classic knife", "falchion knife", "falchion", "gut knife", "hunstmas knife", "karambit", "navaja", "m9 bayonet", "nomad knife", "paracord", "shadow daggers", "stiletto", "survival", "talon", "ursus", "glock", "knife", "huntsman"]
+let weapons = ["deagle", "ump", "stattrak", "™", "skins", "skin", "factory new", "factorynew", "factory-new", "minimal-wear", "minimal wear", "min-wear", "min wear", "minimalwear", "field-tested", "field tested", "fieldtested", "well-worn", "well worn", "wellworn", "battle-scarred", "battle scarred", "battlescarred", "cs go", "cs-go", "csgo", "usp-s", "usp s", "usps", "glock-18", "glock 18", "glock18", "dual berettas", "dual-berettas", "p250", "tec-9", "tec9", "tec 9", "cz75-auto", "cz75 auto", "five-seven", "five seven", "desert eagle", "r8 revolver", "revolver", "mac-10", "mac 10", "mp9", "mp-9", "mp7", "mp-7", "mp5-sd", "mp5 sd", "mp5sd", "ump-45", "ump45", "p90", "p-90", "pp-bizon", "ppbizon", "pp bizon", "nova", "xm1014", "sawed-off", "sawed off", "mag-7", "mag 7", "mag7", "m249", "negev", "galil-ar", "galil ar", "galilar", "famas", "ak-47", "ak47", "ak 47", "m4a4", "m4a1-s", "m4a1", "m4a1 s", "ssg-08", "ssg08", "ssg 08", "scout", "sg-553", "sg 553", "sg553", "aug", "awp", "g3sg1", "scar-20", "scar", "scar20", "scar 20", "skeleton knife", "skeleton", "bayonet knife", "bayonet", "bowie knife", "bowie", "butterfly knife", "butterfly", "classic knife", "falchion knife", "falchion", "gut knife", "hunstmas knife", "karambit", "navaja", "m9 bayonet", "nomad knife", "paracord", "shadow daggers", "stiletto", "survival", "talon", "ursus", "glock", "knife", "huntsman"]
 // ToDO add knives
 
 document.addEventListener("mousemove", (e) => {
@@ -110,8 +110,8 @@ document.body.addEventListener("keydown", (e) => {
             if(el.tagName == "IMG"){
                 running = true
                 console.log("CSSKIN HACk ------- Querying!")
-                skinName.innerHTML = ""
-                preSkinName.innerHTML = "";
+                // skinName.innerHTML = ""
+                // preSkinName.innerHTML = "";
                 imageURL = el.src;
                 sendData(imageURL)
             }
@@ -135,17 +135,17 @@ function sendData(url){
         if (res.ok) {
             return res.json()
         } else {
-            alert("something is wrong")
+            // alert("something is wrong")
         }
     }).then(jsonResponse => {
         console.log(jsonResponse)
         returnedresponse = jsonResponse
 
         // Parse and remove wepaon names:
-        preSkinName.innerHTML = `Raw: ${returnedresponse}`
-        weapons.forEach(weapon => {
-            returnedresponse = returnedresponse.toLocaleLowerCase().replace(weapon, "")
-        });
+        // preSkinName.innerHTML = `Raw: ${returnedresponse}`
+        // weapons.forEach(weapon => {
+        //     returnedresponse = returnedresponse.toLocaleLowerCase().replace(weapon, "")
+        // });
         navigator.clipboard.writeText(returnedresponse)
         skinName.innerHTML = `Final: ${returnedresponse}`;
         console.log(" -____--- PLEASE PASTE INTO ENTRY BOX --___-----")
@@ -153,3 +153,28 @@ function sendData(url){
     }
     ).catch((err) => console.error(err));
 }
+
+
+// Updated look into bot src code.
+
+// src/commands/games/skin_game.py randimo skin at line 45
+// db at src/util/database
+
+// Skin data at res/skin_data.json
+
+// from discord
+//https://images-ext-2.discordapp.net/external/OoVC_XdchJpbtJrVDMBLaIrnCMlMm3Txg4y2-NczXJo/https/steamcommunity-a.akamaihd.net/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgposLOzLhRlxfbGTi5N09ajmoeHksj8NrrHj1Rd6dd2j6fEp9T031Dg_kJlNTvyco-ddQ9tYVDQ_Fa7lO68jMC86MvOynZgvCQr-z-DyKRZB5bD/512fx384f
+
+
+// from db
+//https://steamcommunity-a.akamaihd.net/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgposLOzLhRlxfbGTi5N09ajmoeHksj4OrzZglRd6dd2j6fFrNTw2gHh-UtvMmGgctKVe1dvMgrS_1W-xL_tg8O5tJ2cyiZiuCl0-z-DyOSIRCS_/512fx384f
+
+// Parsed
+// https/steamcommunity-a.akamaihd.net/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgposLOzLhRlxfbGTi5N09ajmoeHksj8NrrHj1Rd6dd2j6fEp9T031Dg_kJlNTvyco-ddQ9tYVDQ_Fa7lO68jMC86MvOynZgvCQr-z-DyKRZB5bD/512fx384f
+
+// db // https/steamcommunity-a.akamaihd.net/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgposLOzLhRlxfbGTi5N09ajmoeHksj8NrrHj1Rd6dd2j6fEp9T031Dg_kJlNTvyco-ddQ9tYVDQ_Fa7lO68jMC86MvOynZgvCQr-z-DyKRZB5bD/512fx384f
+// ds // https/steamcommunity-a.akamaihd.net/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgposLOzLhRlxfbGTi5N09ajmoeHksj8NrrHj1Rd6dd2j6fEp9T031Dg_kJlNTvyco-ddQ9tYVDQ_Fa7lO68jMC86MvOynZgvCQr-z-DyKRZB5bD/512fx384f
+
+
+// https://steamcommunity-a.akamaihd.net/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgposLOzLhRlxfbGTi5N09ajmoeHksj4OrzZglRd6dd2j6fFrNTw2gHh-UtvMmGgctKVe1dvMgrS_1W-xL_tg8O5tJ2cyiZiuCl0-z-DyOSIRCS_/512fx384f
+// https://steamcommunity-a.akamaihd.net/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgposLOzLhRlxfbGTi5N09ajmoeHksj8NrrHj1Rd6dd2j6fEp9T031Dg_kJlNTvyco-ddQ9tYVDQ_Fa7lO68jMC86MvOynZgvCQr-z-DyKRZB5bD/512fx384f
--- server.py
+++ server.py
@@ -2,25 +2,136 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
 import os
+import time
+
+import ctypes
+from ctypes import wintypes
+import time
+
+user32 = ctypes.WinDLL('user32', use_last_error=True)
+
+INPUT_MOUSE    = 0
+INPUT_KEYBOARD = 1
+INPUT_HARDWARE = 2
+
+KEYEVENTF_EXTENDEDKEY = 0x0001
+KEYEVENTF_KEYUP       = 0x0002
+KEYEVENTF_UNICODE     = 0x0004
+KEYEVENTF_SCANCODE    = 0x0008
+
+MAPVK_VK_TO_VSC = 0
+
+# msdn.microsoft.com/en-us/library/dd375731
+VK_TAB  = 0x09
+VK_MENU = 0x12
+
+# C struct definitions
+
+wintypes.ULONG_PTR = wintypes.WPARAM
+
+class MOUSEINPUT(ctypes.Structure):
+    _fields_ = (("dx",          wintypes.LONG),
+                ("dy",          wintypes.LONG),
+                ("mouseData",   wintypes.DWORD),
+                ("dwFlags",     wintypes.DWORD),
+                ("time",        wintypes.DWORD),
+                ("dwExtraInfo", wintypes.ULONG_PTR))
+
+class KEYBDINPUT(ctypes.Structure):
+    _fields_ = (("wVk",         wintypes.WORD),
+                ("wScan",       wintypes.WORD),
+                ("dwFlags",     wintypes.DWORD),
+                ("time",        wintypes.DWORD),
+                ("dwExtraInfo", wintypes.ULONG_PTR))
+
+    def __init__(self, *args, **kwds):
+        super(KEYBDINPUT, self).__init__(*args, **kwds)
+        # some programs use the scan code even if KEYEVENTF_SCANCODE
+        # isn't set in dwFflags, so attempt to map the correct code.
+        if not self.dwFlags & KEYEVENTF_UNICODE:
+            self.wScan = user32.MapVirtualKeyExW(self.wVk,
+                                                 MAPVK_VK_TO_VSC, 0)
+
+class HARDWAREINPUT(ctypes.Structure):
+    _fields_ = (("uMsg",    wintypes.DWORD),
+                ("wParamL", wintypes.WORD),
+                ("wParamH", wintypes.WORD))
+
+class INPUT(ctypes.Structure):
+    class _INPUT(ctypes.Union):
+        _fields_ = (("ki", KEYBDINPUT),
+                    ("mi", MOUSEINPUT),
+                    ("hi", HARDWAREINPUT))
+    _anonymous_ = ("_input",)
+    _fields_ = (("type",   wintypes.DWORD),
+                ("_input", _INPUT))
+
+LPINPUT = ctypes.POINTER(INPUT)
+
+def _check_count(result, func, args):
+    if result == 0:
+        raise ctypes.WinError(ctypes.get_last_error())
+    return args
+
+user32.SendInput.errcheck = _check_count
+user32.SendInput.argtypes = (wintypes.UINT, # nInputs
+                             LPINPUT,       # pInputs
+                             ctypes.c_int)  # cbSize
+
+# Functions
+
+def PressKey(hexKeyCode):
+    x = INPUT(type=INPUT_KEYBOARD,
+              ki=KEYBDINPUT(wVk=hexKeyCode))
+    user32.SendInput(1, ctypes.byref(x), ctypes.sizeof(x))
+
+def ReleaseKey(hexKeyCode):
+    x = INPUT(type=INPUT_KEYBOARD,
+              ki=KEYBDINPUT(wVk=hexKeyCode,
+                            dwFlags=KEYEVENTF_KEYUP))
+    user32.SendInput(1, ctypes.byref(x), ctypes.sizeof(x))
+
+def AltTab():
+    """Press Alt+Tab and hold Alt key for 2 seconds
+    in order to see the overlay.
+    """
+    PressKey(VK_MENU)   # Alt
+    PressKey(VK_TAB)    # Tab
+    ReleaseKey(VK_TAB)  # Tab~
+    time.sleep(2)
+    ReleaseKey(VK_MENU) # Alt~
+
+chardict = {"a": 0x41, "b": 0x42, "c": 0x43, "d": 0x44, "e": 0x45, "f": 0x46, "g": 0x47, "h": 0x48, "i": 0x49, "j": 0x4A, "k": 0x4B, "l": 0x4C, "m": 0x4D, "n": 0x4E, "o": 0x4F, "p": 0x50, "q": 0x51, "r": 0x52, "s": 0x53, "t": 0x54, "u": 0x55, "v": 0x56, "w": 0x57, "x": 0x58, "y": 0x59, "z": 0x5A, "Delete": 0x2E, " ": 0x20}
 
 app = Flask(__name__)
 cors = CORS(app)
+
+import json
+with open("skin_data.json", encoding="utf-8") as f:
+    skin_data = json.load(f)
+
+skins = skin_data["skins"]
+no_wear_skins = skin_data["no_wear_skins"]
 
 @app.route("/receiver", methods=["POST"])
 def postME():
     print("Incoming request.")
     data = request.get_json()
-    download_image(data)
-    if os.path.isfile("image.png"):
-        res = rgs("image.png")
-    else:
-        return jsonify("Image failed to donwload")
-    print("Final answer:", res)
-    if res:
-        data = jsonify(res)
-    else:
-        data = jsonify("None")
-    os.remove("image.png")
+    print("url:", data)
+    # download_image(data)
+    # if os.path.isfile("image.png"):
+    #     res = rgs("image.png")
+    # else:
+    #     return jsonify("Image failed to donwload")
+    # print("Final answer:", res)
+    # if res:
+    #     data = jsonify(res)
+    # else:
+    #     data = jsonify("None")
+    # os.remove("image.png")
+    name = findName(data)
+    print("name is:", name)
+    data = jsonify(name)
     return data
 
 def download_image(url):
@@ -37,6 +148,35 @@ def rgs(image_path):
         return output[1] # skin name
     else:
         return False
+
+from pynput.keyboard import Controller
+kb = Controller()
+
+def findName(url):
+    url = formatUrl(url)
+    for skin in skins:
+        skin = skins[skin]
+        if skin["image_url"] == url:
+            name = skin["formatted_name"].split("|")[1].strip()
+            # nameAsHex = [chardict[char.lower()] for char in name]
+            # [PressKey(key) for key in nameAsHex]
+            kb.type(name)
+            time.sleep(0.2)
+            PressKey(0x0D)
+            time.sleep(1)
+            # commandAsHex = [chardict[char.lower()] for char in "cs skin?"]
+            # [PressKey(key) for key in commandAsHex]
+            kb.type("cs skin?")
+            time.sleep(0.2)
+            PressKey(0x0D)
+            time.sleep(2.5)
+            PressKey(0x2E)
+            time.sleep(0.2)
+            return name
+    return False
+
+def formatUrl(url) -> str:
+    return "https:/" + url.split("https")[2].split("width")[0][:-1]
 
 if __name__ == "__main__":
     app.run()
